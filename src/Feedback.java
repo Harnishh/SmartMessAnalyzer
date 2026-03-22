d008b422b73d984678f6a80dd16ca69296b3bc6f
Harnishh <harnishsankhla261@gmail.com>
2026-03-22 17:12:04 +0530

    initial commit

---
+++ src/Feedback.java
@@ -0,0 +1,13 @@
+public class Feedback {
+    int id;
+    String foodItem;
+    int rating;
+    String comment;
+
+    public Feedback(int id, String foodItem, int rating, String comment) {
+        this.id = id;
+        this.foodItem = foodItem;
+        this.rating = rating;
+        this.comment = comment;
+    }
+}
